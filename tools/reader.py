02a29c2835f87f0985294fc5e28202642ff67261
merwanel <>
2025-12-06 17:01:41 +0100

    tool : added a parameter to the reader keep empty string at the end

--- tools/reader.py
+++ tools/reader.py
@@ -1,7 +1,10 @@
-def read_lines(file_path):
+def read_lines(file_path, stripIt = True):
     """
     Reads a file line by line and yields each line stripped of leading/trailing whitespace.
     """
     with open(file_path, 'r') as f:
         for line in f:
-            yield line.strip()
+            if stripIt :
+                yield line.strip()
+            else :
+                yield line[:-1]
